dbcfd0b899ca4a37983eacc25d335288591ded0f
David Korystov <david.korystov@gmail.com>
2023-07-25 17:58:20 +0200

    Initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,34 @@
+import type { GatsbyConfig } from 'gatsby'
+
+interface GoogleGtagPluginOptions {
+  trackingIds: string[] // Массив строк для трекеров Google Analytics ID
+}
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Tatreal`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: ['GA-TRACKING-ID-1'], // Замените на ваш трекер Google Analytics ID
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+  ],
+}
+
+export default config
